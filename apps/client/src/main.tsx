ada1016a7156823304fa38ff44cbaa904e62911c
WilfredoN <wilfredochannel@gmail.com>
2026-01-10 20:22:18 +0200

    feat: set default access token for Cesium Ion

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Ion } from 'cesium';
+
+Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ION_TOKEN;
 
 const queryClient = new QueryClient({
   defaultOptions: {
